5e8797e48e15dd54d0a3248d8eeec93a3b7b9b27
louix233 <louixme@gmail.com>
2026-01-18 18:30:56 +0000

    Add relationship_app models and sample queries

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+
 from django.views.generic.detail import DetailView
 
 from .models import Book
@@ -14,3 +17,15 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+    
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # auto-login after register (optional but useful)
+            return redirect('list_books')
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'relationship_app/register.html', {'form': form})
