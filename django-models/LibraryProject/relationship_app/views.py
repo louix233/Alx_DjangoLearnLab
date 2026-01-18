09a61f7e341b38b4aa045880dec0caa1e4643ca7
louix233 <louixme@gmail.com>
2026-01-18 18:01:39 +0000

    Add relationship_app models and sample queries

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from django.views.generic import DetailView
 
-from .models import Book, Library
+from .models import Book
+from .models import Library
 
 
 def list_books(request):
-    books = Book.objects.all()  # required by checker
+    books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
 
