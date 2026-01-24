ef74efe634769d7d74ad0f099ed40aaa2b705455
louix233 <louixme@gmail.com>
2026-01-24 23:50:18 +0000

    add files

---
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -0,0 +1,8 @@
+# Create your tests here.
+from django import forms
+from .models import Book
+
+class BookForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = ['title', 'author']
