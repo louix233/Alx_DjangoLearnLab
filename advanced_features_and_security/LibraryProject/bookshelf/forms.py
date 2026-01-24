7fcdd49a9267f37786bad3ede121778c0f45139d
louix233 <louixme@gmail.com>
2026-01-24 23:52:20 +0000

    add files

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,8 +1,5 @@
-# Create your tests here.
 from django import forms
-from .models import Book
 
-class BookForm(forms.ModelForm):
-    class Meta:
-        model = Book
-        fields = ['title', 'author']
+class ExampleForm(forms.Form):
+    name = forms.CharField(max_length=100)
+    age = forms.IntegerField(required=False)
