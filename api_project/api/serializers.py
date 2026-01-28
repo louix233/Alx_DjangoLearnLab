fa41c486ee5dd4771bed644c0db4eb42e74a227a
louix233 <louixme@gmail.com>
2026-01-28 23:35:57 +0000

    adding files

---
+++ api_project/api/serializers.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+from rest_framework import serializers
+from .models import Book
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = '__all__'
