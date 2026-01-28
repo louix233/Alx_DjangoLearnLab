c8d6daf20e50cefb894e9cd27299aedb6b51be05
louix233 <louixme@gmail.com>
2026-01-28 23:56:50 +0000

    adding files

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from rest_framework import generics
+from rest_framework import generics, viewsets, permissions
 from .models import Book
 from .serializers import BookSerializer
 
@@ -11,3 +11,4 @@ class BookList(generics.ListAPIView):
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
