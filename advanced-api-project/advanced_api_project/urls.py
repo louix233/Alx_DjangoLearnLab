2d87100488b7ad8ef3e1f0eaa791b6cb32597997
louix233 <louixme@gmail.com>
2026-02-03 01:29:38 +0000

    adding files

--- advanced-api-project/advanced_api_project/urls.py
+++ advanced-api-project/advanced_api_project/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include   # include MUST be imported
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # The checker looks for this EXACT line:
+    path('api/', include('api.urls')),
 ]
