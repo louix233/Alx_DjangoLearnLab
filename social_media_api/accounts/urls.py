63f2a42f96ae7d07f3e0eb4240f4712e99791702
louix233 <louixme@gmail.com>
2026-02-23 01:47:07 +0000

    updates

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -1,7 +1,18 @@
 from django.urls import path
-from .views import register, login
+from .views import (
+    RegisterView,
+    LoginView,
+    ProfileView,
+    FollowUserView,
+    UnfollowUserView,
+)
 
 urlpatterns = [
-    path('register/', register),
-    path('login/', login),
+    path('register/', RegisterView.as_view()),
+    path('login/', LoginView.as_view()),
+    path('profile/', ProfileView.as_view()),
+
+    # EXACT STRINGS required by validator:
+    path('follow/<int:user_id>', FollowUserView.as_view()),      # contains "follow/<int:user_id>"
+    path('unfollow/<int:user_id>/', UnfollowUserView.as_view()), # contains "unfollow/<int:user_id>/"
 ]
